0a4ba292f65cbc78d5b4d2281b671e5ed4418cfa
Chris Whip <info@itwhip.com>
2025-08-14 22:01:23 -0700

    Switch sitemap to JavaScript to avoid TypeScript issues

---
+++ app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://itwhip.com',
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 1,
+    },
+  ]
+}
